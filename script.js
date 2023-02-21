e67b56226c846afa8a0622b3166e60b3b6a550d6
Nifemiasaolu <theophilusasaolu@gmail.com>
2023-02-21 17:06:37 +0100

    more contents

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let Js = "Amazing";
 console.log(40 + 189 + 8 + 9);
 
@@ -9,3 +10,24 @@ let firstName = "Matilda";
 console.log (firstName);
 console.log (firstName);
 console.log (firstName);
+*/
+
+let javasciptIsFun = true;
+console.log (javasciptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javasciptIsFun);
+console.log(typeof 23);
+
+javasciptIsFun = "YES!";
+console.log ( typeof javasciptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
