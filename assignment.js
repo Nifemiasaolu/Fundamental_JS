b712f16902afc422c68a779168457d893270b995
Nifemiasaolu <theophilusasaolu@gmail.com>
2023-02-21 17:06:21 +0100

    More assignments

--- assignment.js
+++ assignment.js
@@ -1,3 +1,5 @@
+// Variables
+
 let country  = "Nigeria";
 let continent = "Africa";
 let population = "200 million"
@@ -5,3 +7,19 @@ let population = "200 million"
 console.log (country);
 console.log (continent);
 console.log (population);
+
+// Data Types 
+
+let isIsIsland = "Nigeria";
+
+isIsIsland = true;
+
+console.log (isIsIsland);
+
+let language;
+
+console.log (typeof isIsIsland);
+console.log (typeof country);
+console.log(typeof population);
+console.log(typeof language);
+
