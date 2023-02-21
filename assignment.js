bec179acd43c12ae0beebc49b146827a940e2d9b
Nifemiasaolu <theophilusasaolu@gmail.com>
2023-02-21 16:14:53 +0100

    Initial commit

---
+++ assignment.js
@@ -0,0 +1,7 @@
+let country  = "Nigeria";
+let continent = "Africa";
+let population = "200 million"
+
+console.log (country);
+console.log (continent);
+console.log (population);
---
+++ script.js
@@ -0,0 +1,11 @@
+let Js = "Amazing";
+console.log(40 + 189 + 8 + 9);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log (firstName);
+console.log (firstName);
+console.log (firstName);
