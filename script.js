651430d47655ab231b0530f051339be93220b69f
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-04-11 14:26:19 +0100

    learning new infos

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-/*
-let Js = "Amazing";
-console.log(40 + 189 + 8 + 9);
-
-console.log("Jonas");
-console.log(23);
-
-let firstName = "Matilda";
-
+var firstName = 'John';
+var age = 28
 console.log (firstName);
-console.log (firstName);
-console.log (firstName);
-*/
 
-let javasciptIsFun = true;
-console.log (javasciptIsFun);
+var job, isMarried;
+job = 'teacher'
+isMarried = false 
 
-// console.log(typeof true);
-console.log(typeof javasciptIsFun);
-console.log(typeof 23);
-
-javasciptIsFun = "YES!";
-console.log ( typeof javasciptIsFun);
-
-let year;
-console.log(year);
-console.log(typeof year);
-
-year = 1991;
-console.log(year);
-console.log(typeof year);
-
-console.log(typeof null);
+console.log (firstName + ' is a ' + age + ' years old ' 
++ job + '. Is he married? ' + isMarried)
