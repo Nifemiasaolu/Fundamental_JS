c6c6d4d8bc5c9f87f5e9eb47fad4ff3c83138ab0
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-04-11 14:54:31 +0100

    Double the commit

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ var age = 28
 console.log (firstName);
 
 var job, isMarried;
-job = 'teacher'
-isMarried = false 
+job = 'teacher';
+isMarried = false;
 
 console.log (firstName + ' is a ' + age + ' years old ' 
-+ job + '. Is he married? ' + isMarried)
++ job + '. Is he married? ' + isMarried);
+
+age = 'twenty-eight';
+job = 'engineer';
+
+alert (firstName + ' is a ' + age + ' years old ' 
++ job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('what is his last Name?');
+console.log (firstName + ' ' + lastName);
