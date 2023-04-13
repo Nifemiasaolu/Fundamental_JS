5cf4a4669d4f240f4554bdbd519c37fd06044791
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-04-13 16:12:24 +0100

    new details

--- script.js
+++ script.js
@@ -19,12 +19,54 @@
 // console.log (firstName + ' ' + lastName);
 
 
-let firstName = 'Matilda';
-console.log (firstName);
-console.log (firstName);
-console.log (firstName);
+// let firstName = 'Matilda';
+// console.log (firstName);
+// console.log (firstName);
+// console.log (firstName);
 
-let myFirstJob = 'Programmer';
-let myCurrentJob = 'Teacher';
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
 
-console.log (myFirstJob);
+// console.log (myFirstJob);
+
+// console.log (typeof null); 
+
+// const birthYear = 1990;
+
+
+    // Basic Operators
+
+    // Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarwah = now - 2020;
+console.log (ageJonas, ageSarwah);
+
+
+console.log (ageJonas * 2, ageJonas / 10, 2**3);
+
+// 2 ** 3 means 2 to the power of 3 = 2*2*2
+
+const firstName = 'Jonas';
+const lastName = 'Schne';
+console.log (firstName + ' ' + lastName);
+
+
+    // Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25 
+
+x *=4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--; // x = x - 1
+console.log (x);
+
+    // Comparison operators
+
+console.log (ageJonas > ageSarwah); // >,<, >=, <=
+console.log (ageSarwah >= 18);
+
+const isFullAge = ageSarwah >= 18;
+
+console.log (now - 1991 > now - 2018);
