cea8e2a0cdb0c1c27fe55d34ed616fb2ac3d005b
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-04-13 16:10:25 +0100

    some assignment

--- assignment.js
+++ assignment.js
@@ -1,5 +1,5 @@
-// let country = 'Nigeria';
-// let continent = 'Africa';
+let country = 'Nigeria';
+let continent = 'Africa';
 // let population = '200 million';
 
 // console.log (country);
@@ -21,20 +21,50 @@
 // let language = 'Pidgin';
 // console.log (language);
 
-let javaScriptIsFun = true;
-console.log (javaScriptIsFun);
+// let javaScriptIsFun = true;
+// console.log (javaScriptIsFun);
 
-javaScriptIsFun = "YES!";
-console.log (javaScriptIsFun);
+// javaScriptIsFun = "YES!";
+// console.log (javaScriptIsFun);
 
-console.log (typeof javaScriptIsFun);
+// console.log (typeof javaScriptIsFun);
 
-let year;
+// let year;
 
-console.log (year);
-console.log (typeof year);
+// console.log (year);
+// console.log (typeof year);
 
-year = 1990;
-console.log (year);
-console.log (typeof year);
+// year = 1990;
+// console.log (year);
+// console.log (typeof year);
 
+    // Let Const and Var Section
+
+// let language = 'Yoruba';
+
+// const country = false;
+
+// country = true;
+// console.log (Nigeria);
+
+
+    // Basic Operators
+const language = "Yoruba"
+const population = 200000 ;
+console.log (population / 2);
+
+// population++;
+console.log (population);
+
+const finlandPopulation = 6000;
+
+console.log (finlandPopulation > population);
+
+console.log (population > finlandPopulation);
+
+console.log (population / 2 < 33);
+
+const description = country + " is in " + continent + ", and its "
++ population + " people speak " + language
+
+console.log (description);
