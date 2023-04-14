e7ae4a5adc5402e1f8d8b1794ae3248da17d530a
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-04-14 18:25:58 +0100

    new topics

--- script.js
+++ script.js
@@ -37,36 +37,40 @@
     // Basic Operators
 
     // Math Operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarwah = now - 2020;
+// console.log (ageJonas, ageSarwah);
+
+
+// console.log (ageJonas * 2, ageJonas / 10, 2**3);
+
+// // 2 ** 3 means 2 to the power of 3 = 2*2*2
+
+// const firstName = 'Jonas';
+// const lastName = 'Schne';
+// console.log (firstName + ' ' + lastName);
+
+
+//     // Assignment Operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25 
+
+// x *=4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+// x--; // x = x - 1
+// console.log (x);
+
+//     // Comparison operators
+
+// console.log (ageJonas > ageSarwah); // >,<, >=, <=
+// console.log (ageSarwah >= 18);
+
+// const isFullAge = ageSarwah >= 18;
+
+
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarwah = now - 2020;
-console.log (ageJonas, ageSarwah);
-
-
-console.log (ageJonas * 2, ageJonas / 10, 2**3);
-
-// 2 ** 3 means 2 to the power of 3 = 2*2*2
-
-const firstName = 'Jonas';
-const lastName = 'Schne';
-console.log (firstName + ' ' + lastName);
-
-
-    // Assignment Operators
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25 
-
-x *=4; // x = x * 4 = 100
-x++; // x = x + 1
-x--; // x = x - 1
-x--; // x = x - 1
-console.log (x);
-
-    // Comparison operators
-
-console.log (ageJonas > ageSarwah); // >,<, >=, <=
-console.log (ageSarwah >= 18);
-
-const isFullAge = ageSarwah >= 18;
-
 console.log (now - 1991 > now - 2018);
